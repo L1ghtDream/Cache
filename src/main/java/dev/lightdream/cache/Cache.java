84eb45a432db46d097315225df8c388ce46abb95
LightDream <radu.voinea.mihai@gmail.com>
2022-04-20 23:54:02 +0300

    Project detached

---
+++ src/main/java/dev/lightdream/cache/Cache.java
@@ -0,0 +1,50 @@
+package dev.lightdream.cache;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.function.Consumer;
+
+public class Cache<T> {
+
+    public long updatePeriod;
+    public T value;
+    public Consumer<Cache<T>> updater;
+    public boolean enabled = true;
+
+    public Cache(Consumer<Cache<T>> updater, long updatePeriod) {
+        this.updater = updater;
+        this.updatePeriod = updatePeriod;
+        registerUpdater();
+        update();
+    }
+
+    public void update() {
+        this.updater.accept(this);
+    }
+
+    public void update(T value) {
+        this.value = value;
+    }
+
+    public void registerUpdater() {
+        TimerTask task = new TimerTask() {
+            public void run() {
+                if (enabled) {
+                    update();
+                }
+            }
+        };
+        Timer timer = new Timer();
+
+        timer.schedule(task, 0, updatePeriod);
+    }
+
+    public T get() {
+        return value;
+    }
+
+    public void cancel() {
+        enabled = false;
+    }
+
+}
